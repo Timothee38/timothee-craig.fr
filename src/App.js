426b97976a9517ee43ffd310dd5908b200dc484c
Timothee Craig <timotheecraig@Timothees-MacBook-Pro.local>
2018-09-01 10:41:08 +0200

    Update images path retrieval

--- src/App.js
+++ src/App.js
@@ -20,17 +20,17 @@ class App extends Component {
     axios
       .get(this.baseURL + "frontpage")
       .then(response => {
-        // create a new "State" object without mutating 
-        // the original State object. 
+        // create a new "State" object without mutating
+        // the original State object.
         const newState = Object.assign({}, this.state, {
           frontpage: response.data
         });
         // store the new state object in the component's state
         this.setState(newState);}).catch(error => console.log(error));
-    
+
     axios
       .get(this.baseURL + "contact")
-      .then(response => { 
+      .then(response => {
         const newState = Object.assign({}, this.state, {
           contact: response.data
         });
@@ -40,7 +40,7 @@ class App extends Component {
 
     axios
       .get(this.baseURL + "projects")
-      .then(response => { 
+      .then(response => {
         const newState = Object.assign({}, this.state, {
           projects: response.data
         });
@@ -50,8 +50,8 @@ class App extends Component {
 
     axios
         .get(this.baseURL + "images")
-        .then(response => { 
-          document.getElementById('root').style.background = "#D9D6CA url('"+ response.data.path +"') no-repeat center center fixed";
+        .then(response => {
+          document.getElementById('root').style.background = "#D9D6CA url('./img/"+ response.data.path +"') no-repeat center center fixed";
         }).catch(error => console.log(error));
 
     axios.post(this.baseURL + "logs", { date: new Date(), lang: navigator.language, logType: 1, link: null }).then(response => {}).catch(error => console.log(error));

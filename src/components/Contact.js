8a9678b08eb3e69e4e4cb40e54da3b96ac744c0f
Timothee Craig <timotheecraig@Timothees-MacBook-Pro.local>
2018-09-01 18:15:08 +0200

    target blank on links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Contact(props) {
     if(props.link != null) {
-        return <li><a href={props.link}>{props.enTitle}</a></li>;
-    } 
-    return <li>{props.enTitle}</li>; 
+        return <li><a target="_blank" rel="noopener noreferrer" href={props.link}>{props.enTitle}</a></li>;
+    }
+    return <li>{props.enTitle}</li>;
 }
 
 export default Contact;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,10 +3,10 @@ import React from "react";
 function Projects(props) {
     if(props.link!=null) {
         return (<li>
-            <a href={props.link}>{props.enTitle}</a>
+            <a target="_blank" rel="noopener noreferrer" href={props.link}>{props.enTitle}</a>
             <p>{props.enDesc}</p>
         </li>);
-    } 
+    }
     return (<li>
         {props.enTitle}
         <p>{props.enDesc}</p>
